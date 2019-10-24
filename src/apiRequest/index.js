daf8b422c8786280c7434d9f4e27c6e0288d031c
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-10-24 22:20:50 +0600

    apirequest clean up

--- src/apiRequest/index.js
+++ src/apiRequest/index.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 
 let baseURL;
 
+const api = '/api/v1';
+
 if (process.env.NODE_ENV === 'production') {
-  baseURL = 'https://hr-bot-server.herokuapp.com/api/v1/';
+  baseURL = `https://hr-bot-server.herokuapp.com${api}`;
 } else {
-  baseURL = 'http://localhost:3001/api/v1/';
+  baseURL = `http://localhost:3001${api}`;
 }
 
 export default axios.create({
