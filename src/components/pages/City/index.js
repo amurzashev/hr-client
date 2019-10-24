5feda4777f75a47d41be14c1375ed86c9936682d
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-10-24 18:20:54 +0600

    city api call

--- src/components/pages/City/index.js
+++ src/components/pages/City/index.js
@@ -1,12 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import apiRequest from 'apiRequest';
+import Status from 'components/atoms/Status';
 
 const City = props => {
+  const [weather, setWeather] = useState({});
+  const [status, setStatus] = useState('loading');
   useEffect(() => {
     apiRequest(`current/${props.match.params.coordinates}`)
-    .then(resp => console.log(resp.data))
+    .then(resp => {
+      setWeather(resp.data);
+    })
     .catch(err => console.log(err.response.data));
   }, [props.match.params.coordinates]);
+  if (Object.entries(weather).length === 0) return <Status type={status} />
   return <p>city info</p>
 };
 
