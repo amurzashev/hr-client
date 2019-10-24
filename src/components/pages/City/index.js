c354b68f40080b6adf8cb0f0792736af22100598
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-10-24 18:13:47 +0600

    city page setup

--- src/components/pages/City/index.js
+++ src/components/pages/City/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import apiRequest from 'apiRequest';
 
-const City = () => {
+const City = props => {
+  useEffect(() => {
+    apiRequest(`current/${props.match.params.coordinates}`)
+    .then(resp => console.log(resp.data))
+    .catch(err => console.log(err.response.data));
+  }, [props.match.params.coordinates]);
   return <p>city info</p>
 };
 
