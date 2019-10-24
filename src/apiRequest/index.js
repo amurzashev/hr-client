8289a1d76226edf3ca8804b6ec7d4b13f932aeb1
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-10-24 19:46:25 +0600

    prepared for heroku

--- src/apiRequest/index.js
+++ src/apiRequest/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let baseURL;
 
 if (process.env.NODE_ENV === 'production') {
-  baseURL = 'prod url';
+  baseURL = 'https://hr-bot-server.herokuapp.com/api/v1/';
 } else {
   baseURL = 'http://localhost:3001/api/v1/';
 }
