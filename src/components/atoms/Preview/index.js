ecf2b5b8414f5d09657cde58e76d77c47566d070
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-10-24 17:32:33 +0600

    preview and gallery css

--- src/components/atoms/Preview/index.js
+++ src/components/atoms/Preview/index.js
@@ -11,17 +11,30 @@ const Card  = styled(Link)`
   background: ${props => props.theme.colors.primary.pacific};
   color: ${props => props.theme.colors.primary.white};
   padding: 20px;
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   cursor: pointer;
+  margin-bottom: 20px;
   ${props => props.theme.breakpoints.mobile} {
     width: 100%;
     height: 180px;
+  }
+  & > p:first-of-type {
+    margin-bottom: 8px;
+  }
+  & > p {
+    text-align: center;
   }
 `;
 
 const Preview = ({ city }) => {
   return (
     <Card to={`/city/${city.lat},${city.lng}`}>
-      <Caption>{city.name}, {city.countryCode}</Caption>
+      <Caption size='xl'>{city.name}</Caption>
+      <Caption size='xl'>{city.countryName}</Caption>
     </Card>
   )
 };
--- src/components/molecules/Gallery/index.js
+++ src/components/molecules/Gallery/index.js
@@ -4,11 +4,15 @@ import styled from '@emotion/styled';
 
 const Wrap = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  flex-wrap: wrap;
   align-items: center;
+  justify-content: space-between;
   padding: 20px;
-  & > a:not(:last-child) {
-    margin-bottom: 20px;
+  width: 940px;
+  margin: auto;
+  ${props => props.theme.breakpoints.mobile} {
+    width: 100%;
   }
 `;
 
